77267909beab1bf73b609d645d55d9534743ad4d
Yousef El-Kurdi <yelkurdi@gmail.com>
2025-10-15 02:59:49 -0400

    updates

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,54 @@
+-- OPTIONS
+local set = vim.opt
+
+-- line nums
 vim.opt.number = true
+
+-- indentation and tabs
+set.tabstop = 4
+set.shiftwidth = 4
+set.autoindent = true
+set.expandtab = true
+
+-- search settings
+set.ignorecase = true
+set.smartcase = true
+
+-- appearance
+set.termguicolors = true
+set.background = "dark"
+set.signcolumn = "yes"
+
+-- cursor line
 vim.opt.cursorline = true
-vim.opt.shiftwidth = 4
+
+-- 80th column
+-- set.colorcolumn = "80"
+
+-- clipboard
+-- set.clipboard:append("unnamedplus")
+
+-- backspace
+set.backspace = "indent,eol,start"
+
+-- split windows
+set.splitbelow = true
+set.splitright = true
+
+-- dw/diw/ciw works on full-word
+set.iskeyword:append("-")
+
+-- keep cursor at least 8 rows from top/bot
+set.scrolloff = 8
+
+-- undo dir settings
+set.swapfile = false
+set.backup = false
+set.undodir = os.getenv("HOME") .. "/.vim/undodir"
+set.undofile = true
+
+-- incremental search
+set.incsearch = true
+
+-- faster cursor hold
+set.updatetime = 50
