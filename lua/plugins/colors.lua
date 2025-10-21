57b356d329c4a862f2eb1d9f2fa009a11aeb5de5
Yousef El-Kurdi <yelkurdi@gmail.com>
2025-10-21 14:33:34 -0400

    updates colors, snacks, claudecode

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -2,8 +2,21 @@ return {
     {
         "folke/tokyonight.nvim",
         config = function()
+
+            -- Color picker: https://htmlcolorcodes.com/color-picker/
+
             vim.cmd.colorscheme "tokyonight"
-            vim.api.nvim_set_hl(0, "LineNr", { fg = "#808086" }) -- Grey for general line numbers
+
+            vim.api.nvim_set_hl(0, 'Normal', { bg = '#00044F' })
+
+            -- Grey for general line numbers
+            vim.api.nvim_set_hl(0, "LineNr", { fg = "#808086" })
+
+            -- Set the background and foreground of the hover window content
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#20195E" })
+
+            -- Set the background and foreground of the hover window border
+            -- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#4F002B" })
         end
     },
     {
