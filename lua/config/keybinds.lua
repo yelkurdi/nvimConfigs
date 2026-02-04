e9ffee3b81fbcb34a0c88c9b4f1bc5d7b2999547
Yousef El-Kurdi <yelkurdi@gmail.com>
2026-02-04 09:27:45 -0500

    adds left gutter toggle

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -3,3 +3,26 @@ vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 
 -- folding
 vim.api.nvim_set_keymap('n', '<space><space>', 'za', { noremap = true, silent = true })
+
+-- Function to toggle all gutter columns at once
+local function toggle_gutter()
+    if vim.opt.number:get() or vim.opt.relativenumber:get() then
+        -- Turn everything OFF
+        vim.opt.number = false
+        vim.opt.relativenumber = false
+        vim.opt.signcolumn = "no"
+        vim.opt.foldcolumn = "0"
+        print("Gutter Hidden")
+    else
+        -- Turn everything ON (Adjust these to your preferred defaults)
+        vim.opt.number = true
+        vim.opt.relativenumber = false
+        vim.opt.signcolumn = "yes"
+        vim.opt.foldcolumn = "1"
+        print("Gutter Shown")
+    end
+end
+
+-- Create the keymap
+-- Use <leader>tg to trigger the function
+vim.keymap.set('n', '<leader>tg', toggle_gutter, { desc = "Toggle Left Gutter (Numbers/Signs/Folds)" })
